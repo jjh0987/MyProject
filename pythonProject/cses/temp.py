e876fa6df9e7c79bca93e8ccc1af2ecb818ec56e
junho <djflswlstlf@naver.com>
2022-01-20 18:21:05 +0900

    pull-push matching commit

--- pythonProject/cses/temp.py
+++ pythonProject/cses/temp.py
@@ -1,20 +1,40 @@
-N,M,K = map(int,input().split())
+N,M = map(int,input().split())
 A = sorted([int(i) for i in input().split()])
-B = sorted([int(i) for i in input().split()])
-# N+M i,j 이용
+B = [int(i) for i in input().split()]
+# 9 3 9 6 6 8 6 2 6 3
 
-i = 0
-j = 0
-cnt = 0
-while i+j < N+M:
-    try:
-        if abs(B[j]-A[i]) <= K:
-            cnt += 1
-            i += 1
-            j += 1
-        elif B[j]-A[i] > K:
-            i += 1
-        else:
-            j += 1
-    except:break
-print(cnt)
+ans1 = []
+ans2 = []
+for c in B:
+    left_idx = 0
+    right_idx = len(A)-1
+    mid_idx = N//2
+    ans = -1
+    while 1:
+        if left_idx == mid_idx:
+            if A[right_idx] == c:
+                ans += A.pop(right_idx)+1
+                break
+            elif A[left_idx] <= c:
+                if left_idx < 0:
+                    break
+                else:
+                    ans += A.pop(left_idx)+1
+                    break
+            else:
+                break
+        if A[mid_idx] > c:
+            right_idx = mid_idx
+            mid_idx = int((right_idx-mid_idx)/2)
+        elif A[mid_idx] < c:
+            left_idx = mid_idx
+            mid_idx = int((mid_idx-left_idx)/2)
+        elif A[mid_idx] == c:
+            ans += A.pop(mid_idx)+1
+    if ans != -1:
+        ans1.append(ans)
+    else:
+        ans2.append(ans)
+ans1.extend(ans2)
+for i in ans1:
+    print(i)
