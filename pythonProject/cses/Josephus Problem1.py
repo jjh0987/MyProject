abce9f29b754a73121efa545c802af9b5d3e2ebb
전준호 <djflswlstlf@naver.com>
2022-02-18 01:10:39 +0900

    2020-02-18 deque problem

--- pythonProject/cses/Josephus Problem1.py
+++ pythonProject/cses/Josephus Problem1.py
@@ -1,20 +1,17 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-n,k = map(int,input().split())
+n = int(input())
 queue = deque()
 for i in range(n):
     queue.append(i+1)
-if k >= n:
-    k = k % n
-
+k = 2
 ans = ''
 while 1:
     if len(queue) == 0:
+        ans = ans.rstrip(', ')
         print(ans)
         break
-    for i in range(k):
+    for i in range(k-1):
         queue.append(queue.popleft())
     ans += str(queue.popleft())+' '
-
-# test case : 123456 666666
