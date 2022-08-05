5afa129d5086f1ae75f122f7c042a6ab57bf5eef
jjh0987 <djflswlstlf@naver.com>
2022-08-06 01:11:41 +0900

    2022-08-06

---
+++ pythonProject/dacon/rainfall/api.py
@@ -0,0 +1,5 @@
+key = '4662656b786a6a6833304c6f766b78'
+url = f'http://openAPI.seoul.go.kr:8088/{key}/json'
+
+import requests
+from bs4 import BeautifulSoup
--- pythonProject/dacon/rainfall/main.py
+++ pythonProject/dacon/rainfall/main.py
@@ -7,26 +7,39 @@ from scipy import interpolate
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, GRU, AveragePooling1D, GlobalAveragePooling1D
+from tensorflow.keras.layers import Dense, LSTM, GRU, AveragePooling1D, GlobalAveragePooling1D, Dropout, Conv1D
 w_list = sorted(glob("/Users/junho/Downloads/competition_data/water_data/*.csv"))
 w_list
+r_list = sorted(glob("/Users/junho/Downloads/competition_data/rf_data/*.csv"))
+r_list
 
-pd.read_csv(w_list[0])
-
+# setting train
 train_data = []
 train_label = []
 num = 0
 
-for i in w_list[:-1]:
+# tp1 = pd.read_csv(w_list[0])
+# tp1.columns
+# tp2 = pd.read_csv(r_list[0])
+# tp2.columns
+# pd.merge(tp1,tp2,how='inner',on='ymdhm')
 
-    tmp = pd.read_csv(i)
+for i in range(10):
+
+    tmp1 = pd.read_csv(w_list[i])
+    tmp2 = pd.read_csv(r_list[i])
+
+    tmp = pd.merge(tmp1,tmp2,how='inner',on='ymdhm')
+
+    tmp['rain_mean'] = [tmp.loc[j,['rf_10184100','rf_10184110','rf_10184140']].mean() for j in range(len(tmp))]
     tmp = tmp.replace(" ", np.nan)
     tmp = tmp.interpolate(method='values')
     tmp = tmp.fillna(0)
 
     for j in tqdm(range(len(tmp) - 432)):
-        train_data.append(np.array(tmp.loc[j:j + 431, ["swl", "inf", "sfw", "ecpc",
-                                                       "tototf", "tide_level",
+        train_data.append(np.array(tmp.loc[j:j + 431, ['swl','inf','sfw',
+                                                       "tototf",
+
                                                        "fw_1018662", "fw_1018680",
                                                        "fw_1018683", "fw_1019630"]]).astype(float))
 
@@ -35,30 +48,26 @@ for i in w_list[:-1]:
 
 train_data = np.array(train_data)
 train_label = np.array(train_label)
-input_shape = (train_data[0].shape[0], train_data[0].shape[1])
 
-model = Sequential()
-model.add(LSTM(256, input_shape=input_shape))
-model.add(Dense(4, activation = 'relu'))
-
-optimizer = tf.optimizers.RMSprop(0.001)
-model.compile(optimizer=optimizer,loss='mse', metrics=['mae'])
-model.fit(train_data, train_label, epochs=1, batch_size=128)
-
-
+tmp['inf'].mean()
+# setting test
 test_data = []
 test_label = []
-tmp = pd.read_csv(w_list[-1])
+tmp1 = pd.read_csv(w_list[-1])
+tmp2 = pd.read_csv(r_list[-1])
+tmp = pd.merge(tmp1,tmp2,how='inner',on='ymdhm')
+tmp['rain_mean'] = [tmp.loc[j, ['rf_10184100', 'rf_10184110', 'rf_10184140']].mean() for j in range(len(tmp))]
 tmp = tmp.replace(" ", np.nan)
 # 이전값을 사용
 tmp = tmp.fillna(method='pad')
 tmp = tmp.fillna(0)
 
-# tmp.loc[:, ["wl_1018662", "wl_1018680", "wl_1018683", "wl_1019630"]] = tmp.loc[:, ["wl_1018662", "wl_1018680", "wl_1018683", "wl_1019630"]]*100
+tmp.loc[:, ["wl_1018662", "wl_1018680", "wl_1018683", "wl_1019630"]] = tmp.loc[:, ["wl_1018662", "wl_1018680", "wl_1018683", "wl_1019630"]]*100
 
 for j in tqdm(range(4032, len(tmp) - 432)):
-    test_data.append(np.array(tmp.loc[j:j + 431, ["swl", "inf", "sfw", "ecpc",
-                                                  "tototf", "tide_level",
+    test_data.append(np.array(tmp.loc[j:j + 431, ['swl','inf','sfw',
+                                                  "tototf",
+
                                                   "fw_1018662", "fw_1018680",
                                                   "fw_1018683", "fw_1019630"]]).astype(float))
 
@@ -67,7 +76,29 @@ for j in tqdm(range(4032, len(tmp) - 432)):
 
 test_data = np.array(test_data)
 test_label = np.array(test_label)
+
+# setting model
+input_shape = (train_data[0].shape[0], train_data[0].shape[1])
+model = Sequential()
+model.add(AveragePooling1D(input_shape=input_shape))
+model.add(LSTM(256)) # input_shape=input_shape
+# model.add(GRU(32,return_sequences=False)) # input_shape=input_shape
+model.add(Dense(4, activation = 'relu'))
+
+optimizer = tf.optimizers.Adam(0.0005)
+model.compile(optimizer=optimizer,loss='mse', metrics=['mae'])
+model.fit(train_data, train_label, epochs=10, batch_size=128)
+
+
 pred = model.predict(test_data)
 
-pred
+pred = pd.DataFrame(pred)
 
+sample_submission = pd.read_csv("/Users/junho/Downloads/competition_data/sample_submission.csv")
+
+sample_submission["wl_1018662"] = pred[0]
+sample_submission["wl_1018680"] = pred[1]
+sample_submission["wl_1018683"] = pred[2]
+sample_submission["wl_1019630"] = pred[3]
+sample_submission.to_csv("/Users/junho/Downloads/competition_data/submission.csv", index = False)
+print(1)
