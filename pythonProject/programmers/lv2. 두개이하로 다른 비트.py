7df1a60519bc76b23fe57d7228d59fffbbc3005b
JunHo <jjh0987@yonsei.ac.kr>
2023-02-21 20:23:14 +0900

    2023-02-21

--- pythonProject/programmers/lv2. 두개이하로 다른 비트.py
+++ pythonProject/programmers/lv2. 두개이하로 다른 비트.py
@@ -1,12 +1,24 @@
-numbers = [1,2,7,100000]
-bin(10**15)
+numbers = [3,7,11,15]
+
+
 def find(x):
     if x % 4 == 3:
-        return x + 1
+        tar = bin(x)[2:]
+        if '0' in tar:
+            tar = list(tar)
+            tar.reverse()
+            idx0 = tar.index('0')
+            tar[idx0], tar[idx0-1] = tar[idx0-1], tar[idx0]
+            tar.reverse()
+            tar = ''.join(tar)
+            return int(tar, 2)
+        else:
+            tar = '10' + '1' * (len(tar) - 1)
+            return int(tar, 2)
     else:
         return x + 1
 
 
-number = [find(i) for i in range(10**15+1)]
-
-[bin(i)[2:] for i in range(1,50,4)]
+numbers = [find(i) for i in numbers]
+find(1)
+bin(11)
