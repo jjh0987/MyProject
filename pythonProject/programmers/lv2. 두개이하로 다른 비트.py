d28456569b022b066b2573b51d41268398eab2f8
junho <jjh0987@yonsei.ac.kr>
2023-02-21 17:29:50 +0900

    2023-02-21

--- pythonProject/programmers/lv2. 두개이하로 다른 비트.py
+++ pythonProject/programmers/lv2. 두개이하로 다른 비트.py
@@ -1,38 +1,12 @@
-numbers = [2,7]
-
+numbers = [1,2,7,100000]
+bin(10**15)
 def find(x):
-    i = 1
-    tp1 = bin(x)[2:]
+    if x % 4 == 3:
+        return x + 1
+    else:
+        return x + 1
 
-    if x%2 == 0:
-        tp1 = tp1[:-1] + '1'
-        return int(tp1,2)
 
-    while 1:
-        diff = 0
-        tp2 = bin(x+i)[2:]
-        if len(tp2) > len(tp1):
-            tp3 = '0' + tp1
-        else:
-            tp3 = tp1
+number = [find(i) for i in range(10**15+1)]
 
-        for j in range(len(tp3)):
-            if diff > 2:
-                break
-            if tp2[j] != tp3[j]:
-                diff += 1
-
-        if diff <= 2:
-            break
-
-        i += 1
-    return int(tp2,2)
-
-label = [find(i) for i in range(100001)]
-answer = [label[i] for i in numbers]
-label[100000]
-
-find(3)
-
-bin(3)
-bin(4)
+[bin(i)[2:] for i in range(1,50,4)]
