bb9c5be927b8af6263a5060f4d3c79cc46bdb1ce
junho <jjh0987@yonsei.ac.kr>
2023-02-27 17:30:15 +0900

    2023-02-27

--- pythonProject/programmers/lv2. 뒤에 있는 큰 수 찾기.py
+++ pythonProject/programmers/lv2. 뒤에 있는 큰 수 찾기.py
@@ -1,3 +1,29 @@
-numbers = [9, 1, 5, 3, 6, 2]
+numbers = [9, 1, 5, 3, 7, 2, 2]
+# [-1, 5, 6, 6, -1, -1, -1]
+ans = [-1]*len(numbers)
+M = 0
+flag = 0
+for i in range(len(numbers)-1):
+    if numbers[i] < numbers[i+1]:
+        ans[i] = numbers[i+1]
+        if numbers[0] > M:
+            M = max(M,numbers[i+1])
+        else:
+            ans[0] = M
+            flag = 1
 
-ans = []
+i = -1
+while i < len(numbers):
+    try:
+        if ans[i] != -1:
+            while 1:
+                i -= 1
+                ans[i] = ans[i+1]
+                if ans[i] != ans[i-1]:
+                    break
+        i -= 1
+    except:
+        break
+
+if flag == 0:
+    ans[0] = -1
