e86d78514b853e1246bf2e1bfb41f5a791b00b97
JunHo <jjh0987@yonsei.ac.kr>
2023-03-02 20:02:53 +0900

    2023-03-02

--- pythonProject/programmers/lv2. 뒤에 있는 큰 수 찾기.py
+++ pythonProject/programmers/lv2. 뒤에 있는 큰 수 찾기.py
@@ -1,3 +1,38 @@
-numbers = [9, 1, 5, 3, 6, 2]
+numbers = [9, 2, 1, 5, 3, 6, 2]
 
-ans = []
+stack = []
+ans = [-1]
+
+
+while 1:
+    tar = numbers.pop()
+    stack.append(tar)
+    try:
+        if tar > numbers[-1]:
+            numbers.append(stack.pop())
+            stack.reverse()
+            while stack:
+                if ans[-1] != -1:
+                    ans.append(max(ans[-1],stack.pop()))
+                else:
+                    stack.pop()
+                    ans.append(-1)
+            break
+    except:
+        stack.reverse()
+        while stack:
+            if ans[-1] != -1:
+                ans.append(max(ans[-1], stack.pop()))
+            else:
+                stack.pop()
+                ans.append(-1)
+        break
+
+while 1:
+    tar = numbers.pop()
+    stack.append(tar)
+    if tar <= numbers[-1]:
+        numbers.append(stack.pop())
+        while stack:
+            ans.append(stack.pop())
+        break
