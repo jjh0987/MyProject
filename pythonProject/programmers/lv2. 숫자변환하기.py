abfb3dea704031c2f64fb758bfce6936f22d2e43
junho <jjh0987@yonsei.ac.kr>
2023-03-17 17:30:38 +0900

    2023-03-17

--- pythonProject/programmers/lv2. 숫자변환하기.py
+++ pythonProject/programmers/lv2. 숫자변환하기.py
@@ -1,23 +1,16 @@
 x,y,n = 10,40,30
 
 # dp
+dp = [float('inf') for i in range(1000001)]
+dp[x] = 0
 
-dp = [0 for i in range(1000001)]
-dp[x] = 1
+for i in range(x+1,y+1):
+    if i%3 == 0:
+        dp[i] = min(dp[int(i/3)]+1,dp[i-n]+1,dp[i]+1)
+    elif i%2 == 0:
+        dp[i] = min(dp[int(i/2)]+1,dp[i-n]+1,dp[i]+1)
 
-for i in range(x,y+1):
-    if dp[i] != 0:
-        if i%2 == 0:
-            if 2*i == i-n:
-                dp[i] = min(dp[2*i],dp[i+n])
-        else:
-            dp[i] += dp[i] + 1
-
-        if 3*i == i+n:
-            dp[i] = min(dp[3*i],dp[i+n])
-        else:
-            dp[i] += dp[i] + 1
-        dp[n] += dp[i] + 1
-
-dp[x:y]
+dp[x:y+1]
 dp[40]
+
+min(dp[10],100)
