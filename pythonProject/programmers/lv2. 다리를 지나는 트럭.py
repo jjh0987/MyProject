ff3b1c3027dedd0d4168cf0ebb8f7c83cbcaed53
junho <jjh0987@yonsei.ac.kr>
2023-03-24 17:16:35 +0900

    2023-03-24

--- pythonProject/programmers/lv2. 다리를 지나는 트럭.py
+++ pythonProject/programmers/lv2. 다리를 지나는 트럭.py
@@ -4,11 +4,52 @@ truck_weights = [7,4,5,6]
 
 import collections
 
-bridge = []
+bridge = collections.deque([0 for _ in range(bridge_length)])
 truck_weights = collections.deque(truck_weights)
 
 time = 0
-while bridge or truck_weights:
+num = 0
+while num != 0 or truck_weights:
+    try:
+        if num <= weight or len(bridge) <= bridge_length:
+            if not truck_weights:
+                tar = bridge.popleft()
+                num -= tar
+                if tar > 0:
+                    bridge.append(0)
+                else:
+                    bridge.append(tar)
+            else:
+                tar = truck_weights.popleft()
+                num += tar
+                bridge.append(tar)
+                tp = bridge.popleft()
+                if tp > 0:
+                    num -= tp
+                if num > weight:
+                    tar = bridge.pop()
+                    num -= tar
+                    truck_weights.appendleft(tar)
+                    bridge.append(0)
+        else:
+            bridge.pop()
+            bridge.append(0)
+    except:
+        bridge.popleft()
+        bridge.append(0)
+    time += 1
+
+
+
+
+
+
+
+
+
+
+
+
     if not bridge:
         while truck_weights:
             bridge.append(truck_weights.popleft())
