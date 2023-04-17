9f10097c8237142c7c47fcb1f4fb877aa9d73520
jjh0987 <djflswlstlf@naver.com>
2023-04-18 00:28:13 +0900

    2023-04-18

--- pythonProject/programmers/lv2. JadenCase.py
+++ pythonProject/programmers/lv2. JadenCase.py
@@ -1,4 +1,4 @@
-s = "3People  unFollowed me"
+s = "3People   unFollowed me"
 
 ans = [s[0].upper()]
 flag = 0
@@ -9,3 +9,8 @@ for i in range(1,len(s)):
         ans.append(s[i].upper())
 
 ''.join(ans)
+
+s = s.split()
+s = [i.lower() for i in s]
+s = [i[0].upper() + i[1:] for i in s]
+' '.join(s)
