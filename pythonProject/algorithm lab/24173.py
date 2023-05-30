1213bdc4ad0283b8628932adac326cfa38362d49
jjh0987 <djflswlstlf@naver.com>
2023-05-31 00:16:26 +0900

    2023-05-31

--- pythonProject/algorithm lab/24173.py
+++ pythonProject/algorithm lab/24173.py
@@ -1,12 +1,15 @@
 import sys
+import copy
 input = sys.stdin.readline
 n,k = map(int,input().split())
+cnt = copy.deepcopy(k)
 A = [0]
 A.extend(list(map(int,input().split())))
 # A = list(map(int,input().split()))
 ans = 0
+flag = 0
 def heapify(A,k,n):
-    global ans
+    global ans, cnt, flag
     left = 2*k
     right = 2*k+1
     if right <= n:
@@ -22,39 +25,45 @@ def heapify(A,k,n):
     if A[smaller] < A[k]:
         A[k],A[smaller] = A[smaller],A[k]
         heapify(A,smaller,n)
-
-    print(A[k],A[smaller])
+    if ans == cnt:
+        flag = 1
+        print(*sorted([A[k],A[smaller]]))
     return A[k],A[smaller]
 
 def build_min_heap(A,n):
-    global ans
+    global ans, cnt, flag
     for i in range(n//2,0,-1):
-        x,y = heapify(A,i,n)
-        printer(x,y)
         ans += 1
+        x,y = heapify(A,i,n)
+        # printer(x,y)
 
 def heap_sort(A):
-    global ans
+    global ans, cnt, flag
     n = len(A)-1
     build_min_heap(A,n)
     for i in range(n,1,-1):
         A[1],A[i] = A[i],A[1]
         ans += 1
-        printer(A[1],A[i])
+        if ans == cnt:
+            flag = 1
+            print(*sorted([A[1],A[i]]))
+        # printer(A[1],A[i])
         try:
-            x,y = heapify(A,1,i-1)
-            printer(x,y)
+            if flag == 1:
+                return
             ans += 1
+            x,y = heapify(A,1,i-1)
+            # printer(x,y)
         except:
             pass
 
-def printer(x,y):
-    global ans
-    if ans == k-1:
-        tp = sorted([x,y])
-        print(tp[0],tp[1])
+# def printer(x,y):
+#     global ans
+#     if ans == k-1:
+#         tp = sorted([x,y])
+#         print(tp[0],tp[1])
 
 heap_sort(A)
 
-if ans < k:
+if flag == 0:
     print(-1)
