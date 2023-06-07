4e9d62bed63039600b7844f721ecf46097fe569c
jjh0987 <djflswlstlf@naver.com>
2023-06-07 21:49:00 +0900

    2023-06-07

--- pythonProject/algorithm lab/24173.py
+++ pythonProject/algorithm lab/24173.py
@@ -7,9 +7,9 @@ A = [0]
 A.extend(list(map(int,input().split())))
 # A = list(map(int,input().split()))
 ans = 0
-flag = 0
+answer = []
 def heapify(A,k,n):
-    global ans, cnt, flag
+    global ans, cnt, answer
     left = 2*k
     right = 2*k+1
     if right <= n:
@@ -25,45 +25,36 @@ def heapify(A,k,n):
     if A[smaller] < A[k]:
         A[k],A[smaller] = A[smaller],A[k]
         heapify(A,smaller,n)
-    if ans == cnt:
-        flag = 1
-        print(*sorted([A[k],A[smaller]]))
-    return A[k],A[smaller]
+    # if ans == cnt:
+    #     print(*sorted([A[k],A[smaller]]))
+    return list(sorted([A[k],A[smaller]]))
 
 def build_min_heap(A,n):
-    global ans, cnt, flag
+    global ans, cnt, answer
     for i in range(n//2,0,-1):
         ans += 1
-        x,y = heapify(A,i,n)
+        if cnt == ans:
+            print(*heapify(A,i,n))
         # printer(x,y)
 
 def heap_sort(A):
-    global ans, cnt, flag
+    global ans, cnt, answer
     n = len(A)-1
     build_min_heap(A,n)
     for i in range(n,1,-1):
-        A[1],A[i] = A[i],A[1]
-        ans += 1
-        if ans == cnt:
-            flag = 1
-            print(*sorted([A[1],A[i]]))
-        # printer(A[1],A[i])
         try:
-            if flag == 1:
-                return
+            A[1],A[i] = A[i],A[1]
             ans += 1
-            x,y = heapify(A,1,i-1)
+            # if ans == cnt:
+            #     print(*sorted([A[1],A[i]]))
+            if cnt == ans:
+                print(*list(sorted([A[1],A[i]])))
+            # printer(A[1],A[i])
+            ans += 1
+            if cnt == ans:
+                print(*heapify(A,1,i-1))
             # printer(x,y)
-        except:
-            pass
-
-# def printer(x,y):
-#     global ans
-#     if ans == k-1:
-#         tp = sorted([x,y])
-#         print(tp[0],tp[1])
+        except:print(-1)
 
 heap_sort(A)
 
-if flag == 0:
-    print(-1)
