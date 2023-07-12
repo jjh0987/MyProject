c7ba440b4a5beac6118367819eae0fff12272621
JunHo <jjh0987@yonsei.ac.kr>
2023-07-13 01:21:55 +0900

    2023-07-13

--- pythonProject/programmers/lv2. 조이스틱.py
+++ pythonProject/programmers/lv2. 조이스틱.py
@@ -1,7 +1,8 @@
-name = "JEROEN"
+name = "JAN"
 
-name = name.split('A')
-name
+import re
+p = re.compile('[B-Z]+')
+len_a = [len(i) for i in p.split(name)]
 
 from string import ascii_uppercase
 alpha = {ascii_uppercase[i]:i for i in range(len(ascii_uppercase))}
@@ -11,11 +12,29 @@ def updown_min(alp):
     return min(rev_alpha[alp],alpha[alp])
 
 ans = 0
-for i in name:
-    ans += sum([updown_min(j) for j in i])
-ans += len(name)-1
+target = [updown_min(i) for i in name]
+i = 0
+k = 0
 
+conf1 = target[(i-k)%len(target)]
+conf2 = target[(i+k)%len(target)]
+stop = sum(target)
+while stop > 0:
+    ans += target[i]
+    stop -= target[i]
+    target[i] = 0
+    if conf1 == 0 and conf2 == 0:
+        k += 1
+        continue
+    if max(conf1,conf2) == conf1:
+        i -= 1
+        k = 0
+    elif max(conf1,conf2) == conf2:
+        i -= 1
+        k = 0
+    ans += 1
 
+ans-1
 # answer0 = 0
 # i = 0
 # while i < len(name):
