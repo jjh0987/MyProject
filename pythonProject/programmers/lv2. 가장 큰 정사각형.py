10fa345acb1596e21279ebb9202780c3720bde78
JunHo <jjh0987@yonsei.ac.kr>
2023-07-27 20:57:07 +0900

    2023-07-27

--- pythonProject/programmers/lv2. 가장 큰 정사각형.py
+++ pythonProject/programmers/lv2. 가장 큰 정사각형.py
@@ -1,12 +1,13 @@
-board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
-board
-
+board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,0]]
+board = [[1,1,1,0],[1,1,1,0],[1,1,1,1],[1,1,1,1]]
 for i in range(1,len(board)):
     for j in range(1,len(board[0])):
         try:
-            if board[i][j] != 0:
-                if min(board[i-1][j],board[i][j-1],board[i-1][j-1]) > 0:
-                    board[i][j] += board[i-1][j-1]
+            if board[i-1][j-1] != 0:
+                tar = min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1])
+                if tar > 0:
+                    if board[i][j] == 1:
+                        board[i][j] = tar + 1
         except:
             pass
 max([max(i) for i in board])
