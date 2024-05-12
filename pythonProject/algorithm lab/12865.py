236965d60f315b189566d8ae8b8e9fa412e06f15
JunHo <jjh0987@yonsei.ac.kr>
2024-05-13 00:03:20 +0900

    2024-05-13

--- pythonProject/algorithm lab/12865.py
+++ pythonProject/algorithm lab/12865.py
@@ -3,18 +3,43 @@ import math
 
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
+k,n = map(int, input().split())
 bag = []
 for _ in range(n):
-    w, v = map(int, input().split())
-    bag.append([w, v])
+    v, w = map(int, input().split())
+    bag.append([v, w])
+dp = [[math.inf for _ in range(21)] for _ in range(1001)]
+v,w = bag[0]
+for j in range(k+1):
+    dp[j][1] = v*(j//w)
 
-dp = [[0 for _ in range(101)] for _ in range(100001)]
 for i in range(1, n + 1):
-    w, v = bag[i - 1]
+    v, w = bag[i - 1]
     for j in range(k + 1):
-        if j < w:
-            dp[j][i] = dp[j][i - 1]
-        else:
-            dp[j][i] = max(dp[j][i - 1], dp[j - w][i - 1] + v)
+        dp[j][i] = min(dp[j][i - 1], dp[j-w*(j//w)][i] + v*(j//w))
 print(dp[k][n])
+
+for i in range(k+1):
+    print(dp[i][:10])
+
+
+
+
+import sys
+import math
+input = sys.stdin.readline
+k,n = map(int, input().split())
+bag = []
+for _ in range(n):
+    v, w = map(int, input().split())
+    bag.append([v, w])
+
+dp = [math.inf for _ in range(k+101)]
+dp[0] = 0
+for v,w in bag:
+    for i in range(w,k+101):
+        if i > w:
+            dp[i] = min(dp[i],dp[i-w]+v)
+        else:
+            dp[i] = min(dp[i],v*(i//w))
+print(min(dp[k:]))
--- pythonProject/algorithm lab/9084.py
+++ pythonProject/algorithm lab/9084.py
@@ -1,4 +1,19 @@
 import sys
 input = sys.stdin.readline
-t = int(input())
 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int,input().split()))
+    m = int(input())
+    dp = [0 for _ in range(m+1)]
+    dp[0] = 1
+    for i in range(1,m+1):
+        if i % nums[0] == 0:
+            dp[i] += 1
+
+    for num in nums[1:]:
+        for i in range(1,m+1):
+            if i >= num:
+                dp[i] = dp[i-num] + dp[i]
+    print(dp[-1])
