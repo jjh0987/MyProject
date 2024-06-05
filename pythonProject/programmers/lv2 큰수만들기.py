89244e5a9e6f54d1752ea08e68d70c26c3dbbbb8
JunHo <jjh0987@yonsei.ac.kr>
2024-06-05 15:23:35 +0900

    2024-06-05

--- pythonProject/programmers/lv2 큰수만들기.py
+++ pythonProject/programmers/lv2 큰수만들기.py
@@ -1,27 +1,21 @@
-number = '4177252841'
-k = 4
+number = '1237252841'
+k = 5
 
-number = [[number[i], i] for i in range(len(number))]
 import collections
+number = collections.deque([int(i) for i in number])
 
-tp = collections.deque([number.pop()])
-while 1:
-    tar = number.pop()
-    if tar[0] >= tp[0][0]:
-        tp.appendleft(tar)
-    else:
-        number.append(tar)
-        break
-
-import heapq
-
-heapq.heapify(number)
+stack = []
 while k > 0:
-    heapq.heappop(number)
-    k -= 1
-number.sort(key=lambda x: x[1])
-answer = ''
-for i in number:
-    answer += str(i[0])
-while tp:
-    answer += str(tp.popleft()[0])
+    tar = number.popleft()
+    stack.append(tar)
+    while stack[-1] > number[0]:
+        if k == 0:
+            break
+        stack.append(number.popleft())
+        k -= 1
+    else:
+        if stack[-1] == number[0]:
+            stack.append(number.popleft())
+        else:
+            stack.pop()
+            k -= 1
