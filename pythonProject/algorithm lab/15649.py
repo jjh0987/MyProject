075554df28c31b2ae7dd245b069fdb0b12afcbbd
JunHo <jjh0987@yonsei.ac.kr>
2025-03-20 21:47:04 +0900

    2025-03-20

---
+++ pythonProject/algorithm lab/15649.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+n,m = map(int,input().split())
+answer = []
+visited = [False for _ in range(n+1)]
+def back():
+    if len(answer) == m:
+        print(*answer)
+    for i in range(1,n+1):
+        if not visited[i]:
+            answer.append(i)
+            visited[i] = True
+            back()
+            answer.pop()
+            visited[i] = False
+back()            
